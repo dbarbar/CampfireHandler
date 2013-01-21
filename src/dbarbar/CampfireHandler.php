5c203269bfa5f2eabd7e9af5b5d6d20a9615ca1e
David Barbarisi <davidbarbarisi@gmail.com>
2013-01-20 21:23:53 -0500

    changed to 4 space tabs

--- src/dbarbar/CampfireHandler.php
+++ src/dbarbar/CampfireHandler.php
@@ -8,47 +8,40 @@ use Monolog\Logger;
 
 class CampfireHandler extends AbstractHandler
 {
-  protected $campfire;
-
-  /**
-   *
-   *
-   * Campfire $config = array('subdomain' => '', 'room' => '', 'key' => '')
-   * Room is numeric.
-   * @param integer $level  The minimum logging level at which this handler will be triggered
-   * @param Boolean $bubble Whether the messages that are handled can bubble up the stack or not
-   * @param array   $config Pass in the required config params to initalise the Campfire library.
-   */
-  public function __construct($level = Logger::DEBUG, $bubble = true, Campfire $campfire)
-  {
-    $this->level = $level;
-    $this->bubble = $bubble;
-    $this->campfire = $campfire;
-  }
-
-  public function getCampfire()
-  {
-    return $this->campfire;
-  }
-
-  public function handle(array $record)
-  {
+    protected $campfire;
 
     /**
-     * Record looks like this:
-     * return array(
-     *       'message' => $message,
-     *       'context' => $context,
-     *       'level' => $level,
-     *       'level_name' => Logger::getLevelName($level),
-     *       'channel' => 'test',
-     *       'datetime' => \DateTime::createFromFormat('U.u', sprintf('%.6F', microtime(true))),
-     *       'extra' => array(),
-     *   );
+     * Campfire $config = array('subdomain' => '', 'room' => '', 'key' => '')
+     * Room is numeric.
+     * @param integer $level  The minimum logging level at which this handler will be triggered
+     * @param Boolean $bubble Whether the messages that are handled can bubble up the stack or not
+     * @param array   $config Pass in the required config params to initalise the Campfire library.
      */
-    $formatted_message = $record['channel'] . ' - ' . $record['level_name'] . ' - ' . $record['datetime'] . ' - ' . $record['message'];
-    $this->campfire->send($formatted_message);
+    public function __construct($level = Logger::DEBUG, $bubble = true, Campfire $campfire)
+    {
+        $this->level = $level;
+        $this->bubble = $bubble;
+        $this->campfire = $campfire;
+    }
 
-    return $this->bubble;
-  }
+    public function getCampfire()
+    {
+        return $this->campfire;
+    }
+
+    public function handle(array $record)
+    {
+        if (!$this->isHandling($record)) {
+            return false;
+        }
+
+        try {
+            $this->campfire->send($this->getFormatter()->format($record));
+        } catch (\Exception $e) {
+            // eat the error but return false so the record will bubble.
+            return false;
+        }
+
+        return $this->bubble;
+    }
 }
--- tests/dbarbar/CampfireHandlerTest.php
+++ tests/dbarbar/CampfireHandlerTest.php
@@ -5,50 +5,51 @@ use Monolog\Logger;
 
 class CampfireHandlerTest extends PHPUnit_Framework_TestCase
 {
-  protected $campfire;
+    protected $campfire;
 
-  public function setUp()
-  {
-    // I'm confused why this won't tkae constructor arguments.
-    // The real class wants a config array.
-    $this->campfire = $this->getMock('rcrowe\campfire', array('send'));
-  }
-  public function testCampfireHandlerIsMonologHandler()
-  {
-    $this->assertInstanceOf('Monolog\Handler\HandlerInterface', new CampfireHandler(Logger::WARNING, false, $this->campfire));
-  }
+    public function setUp()
+    {
+        // I'm confused why this won't tkae constructor arguments.
+        // The real class wants a config array.
+        $this->campfire = $this->getMock('rcrowe\campfire', array('send'));
+    }
 
-  public function testCampfireHandlerConstruct()
-  {
-    $handler = new CampfireHandler(Logger::WARNING, false, $this->campfire);
+    public function testCampfireHandlerIsMonologHandler()
+    {
+        $this->assertInstanceOf('Monolog\Handler\HandlerInterface', new CampfireHandler(Logger::WARNING, false, $this->campfire));
+    }
 
-    $this->assertEquals(Logger::WARNING, $handler->getLevel());
-    $this->assertFalse($handler->getBubble());
-    $this->assertSame($this->campfire, $handler->getCampfire());
-  }
+    public function testCampfireHandlerConstruct()
+    {
+        $handler = new CampfireHandler(Logger::WARNING, false, $this->campfire);
 
-  public function testCampfireHandlerHandle()
-  {
-    $channel = 'testing';
-    $message = 'Hello there';
-    $level = Logger::INFO;
-    $levelName = Logger::getLevelName($level);
-    $time = 1234;
-    $record = array(
-      'message' => $message,
-      'context' => array(),
-      'level' => $level,
-      'level_name' => $levelName,
-      'channel' => $channel,
-      'datetime' => $time,
-      'extra' => array(),
-    );
+        $this->assertEquals(Logger::WARNING, $handler->getLevel());
+        $this->assertFalse($handler->getBubble());
+        $this->assertSame($this->campfire, $handler->getCampfire());
+    }
 
-    $formatted_message = "$channel - $levelName - $time - $message";
-    $this->campfire->expects($this->once())
-                     ->method('send')
-                     ->with($this->equalTo($formatted_message));
-    $handler = new CampfireHandler(Logger::WARNING, false, $this->campfire);
-    $handler->handle($record);
-  }
+    public function testCampfireHandlerHandle()
+    {
+        $channel = 'testing';
+        $message = 'Hello there';
+        $level = Logger::ALERT;
+        $levelName = Logger::getLevelName($level);
+        $time = 1234;
+        $record = array(
+            'message' => $message,
+            'context' => array(),
+            'level' => $level,
+            'level_name' => $levelName,
+            'channel' => $channel,
+            'datetime' => $time,
+            'extra' => array(),
+        );
+
+        $formatted_message = "[$time] $channel.$levelName: $message [] []\n";
+        $this->campfire->expects($this->once())
+                         ->method('send')
+                         ->with($this->equalTo($formatted_message));
+        $handler = new CampfireHandler(Logger::WARNING, false, $this->campfire);
+        $handler->handle($record);
+    }
 }
